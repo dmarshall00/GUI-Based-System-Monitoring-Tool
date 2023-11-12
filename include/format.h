e49d8578963c6e11539cf13576204ab87c858e95
dmarshall00 <88748477+dmarshall00@users.noreply.github.com>
2023-11-12 17:44:18 -0500

    Add time formatting

---
+++ include/format.h
@@ -0,0 +1,11 @@
+// creation of format macro (used to define constants)
+#ifndef FORMAT_H
+#define FORMAT_H
+
+#include <string>
+
+namespace Format {
+    std:: string TimePassed(long times);
+};
+
+#endif
