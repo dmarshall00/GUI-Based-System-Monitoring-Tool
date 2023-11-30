732277da5b7743b5c7bf78421dab34f3109b4038
XavierMedy <87413644+XavierMedy@users.noreply.github.com>
2023-11-29 19:08:59 -0500

    Update ncurses_display.h

--- include/ncurses_display.h
+++ include/ncurses_display.h
@@ -10,9 +10,9 @@
 // defining a namespace for NCursesDisplay
 // namespaces used to organize code into logical groups
 namespace NCursesDisplay {
-    void Display(System &system, int n = 10);
+    void Display(System &system);
     void DisplaySystem(System &system, WINDOW *window);
-    void DisplayProcesses(std:: vector<Process> &processes, WINDOW *window, int n);
+    //void DisplayProcesses(std:: vector<Process> &processes, WINDOW *window, int n);
     std::string ProgressBar(float percent);
 };
 
