696a62d2411d1bce25f570782a5ef7146da2437e
XavierMedy <87413644+XavierMedy@users.noreply.github.com>
2023-11-29 19:13:20 -0500

    Update ncurses_display.cpp

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -56,39 +56,7 @@ void NCursesDisplay::DisplaySystem(System &system, WINDOW *window) {
   wrefresh(window);
 }
 
-/*void NCursesDisplay::DisplayProcesses(std::vector<Process> &processes,
-                                      WINDOW *window, int n) {
-  int row{0};
-  int const pid_column{2};
-  int const user_column{9};
-  int const cpu_column{16};
-  int const ram_column{26};
-  int const time_column{35};
-  int const command_column{46};
-  wattron(window, COLOR_PAIR(2));
-  
-  // how the information will look in the window
-  mvwprintw(window, ++row, pid_column, "PID");
-  mvwprintw(window, row, user_column, "USER");
-  mvwprintw(window, row, cpu_column, "CPU[%%]");
-  mvwprintw(window, row, ram_column, "RAM[MB]");
-  mvwprintw(window, row, time_column, "TIME+");
-  mvwprintw(window, row, command_column, "COMMAND");
-  wattroff(window, COLOR_PAIR(2));
 
-  //go through each index in the vector and display the pid, user, cpu, ram, and up time
-  for (int i = 0; i < n; ++i) {
-    mvwprintw(window, ++row, pid_column, to_string(processes[i].Pid()).c_str());
-    mvwprintw(window, row, user_column, processes[i].User().c_str());
-    float cpu = processes[i].getCpuUtilization() * 100;
-    mvwprintw(window, row, cpu_column, to_string(cpu).substr(0, 4).c_str());
-    mvwprintw(window, row, ram_column, processes[i].Ram().c_str());
-    mvwprintw(window, row, time_column,
-              Format::ElapsedTime(processes[i].UpTime()).c_str());
-    mvwprintw(window, row, command_column,
-              processes[i].Command().substr(0, window->_maxx - 46).c_str());
-  }
-}*/
 
 void NCursesDisplay::Display(System &system) {
   initscr();     // start ncurses
@@ -98,18 +66,17 @@ void NCursesDisplay::Display(System &system) {
 
   int x_max{getmaxx(stdscr)};
   WINDOW *system_window = newwin(9, x_max - 1, 0, 0); //creates windows for process and system information
- // WINDOW *process_window =
-     // newwin(3 + n, x_max - 1, system_window->_maxy + 1, 0);
+ 
 
   while (1) {  //infinite loop to constantly display the system
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     box(system_window, 0, 0);   //box encapsulates both windows to show seperation in UI
-    //box(process_window, 0, 0);
+    
     DisplaySystem(system, system_window);
-    //DisplayProcesses(system.Processes(), process_window, n);
+   
     wrefresh(system_window); //refresh both windows to display updated information
-   // wrefresh(process_window);
+   
     refresh();
     std::this_thread::sleep_for(std::chrono::seconds(1));  //pause for 1 sec
   }
