c6d928d7b8cb98c45e2e850e3d2ab20ea1f6dcd2
XavierMedy <87413644+XavierMedy@users.noreply.github.com>
2023-11-29 19:13:40 -0500

    Update ncurses_display.h

--- include/ncurses_display.h
+++ include/ncurses_display.h
@@ -12,7 +12,6 @@
 namespace NCursesDisplay {
     void Display(System &system);
     void DisplaySystem(System &system, WINDOW *window);
-    //void DisplayProcesses(std:: vector<Process> &processes, WINDOW *window, int n);
     std::string ProgressBar(float percent);
 };
 
